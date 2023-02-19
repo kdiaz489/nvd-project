48c3e3b3b2bbef5acc32ffacd36f10326c8ea938
Karen Diaz <kstefdiaz@gmail.com>
2023-02-18 19:18:09 -0800

    updated button inside index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,21 @@
+import { useRouter } from "next/router";
 import { Inter } from "@next/font/google";
 import { Button, Center } from "@chakra-ui/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/dashboard");
+  };
+
   return (
     <Center h="100vh" w="100vw">
-      <Button colorScheme="blue">Sign In</Button>
+      <Button colorScheme="blue" onClick={handleClick}>
+        Go to Dashboard
+      </Button>
     </Center>
   );
 }
