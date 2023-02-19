858c5fc484a339db6277f3b4336a698292875b06
Karen Diaz <kstefdiaz@gmail.com>
2023-02-18 21:04:29 -0800

    downgrading to next 12

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import { useRouter } from "next/router";
-import { Inter } from "@next/font/google";
 import { Button, Center } from "@chakra-ui/react";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const router = useRouter();
